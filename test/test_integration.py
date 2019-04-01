7ad9bcbb487a6240e847f215b124af9791a0e45b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-04-01 15:08:28 +0100

    Port tests to pytest

--- test/test_integration.py
+++ test/test_integration.py
@@ -1,4 +1,3 @@
-from unittest import TestCase
 import io
 from contextlib import redirect_stdout
 import urllib
@@ -7,70 +6,67 @@ import json
 
 import orderly_web
 
+def test_status_when_not_running():
+    cfg = orderly_web.read_config("example")
+    st = orderly_web.status(cfg)
+    assert st.containers["orderly"]["status"] == "missing"
+    assert st.containers["web"]["status"] == "missing"
+    assert st.volumes["orderly"]["status"] == "missing"
+    assert st.network["status"] == "down"
 
-class TestExample(TestCase):
+def test_status_representation_is_str():
+    cfg = orderly_web.read_config("example")
+    st = orderly_web.status(cfg)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        print(st)
+    out = f.getvalue()
+    assert str(st).strip() == out.strip()
+    # __repr__ is called when the object is printed
+    assert st.__repr__() == str(st)
 
-    def test_status_when_not_running(self):
-        cfg = orderly_web.read_config("example")
+def test_start_and_stop():
+    cfg = orderly_web.read_config("example")
+    try:
+        res = orderly_web.start(cfg)
+        assert res
         st = orderly_web.status(cfg)
-        self.assertEqual(st.containers["orderly"]["status"], "missing")
-        self.assertEqual(st.containers["web"]["status"], "missing")
-        self.assertEqual(st.volumes["orderly"]["status"], "missing")
-        self.assertEqual(st.network["status"], "down")
+        assert st.containers["orderly"]["status"] == "running"
+        assert st.containers["web"]["status"] == "running"
+        assert st.volumes["orderly"]["status"] == "created"
+        assert st.network["status"] == "up"
 
-    def test_status_representation_is_str(self):
-        cfg = orderly_web.read_config("example")
-        st = orderly_web.status(cfg)
         f = io.StringIO()
         with redirect_stdout(f):
-            print(st)
-        out = f.getvalue()
-        self.assertEqual(str(st).strip(), out.strip())
-        # __repr__ is called when the object is printed
-        self.assertEqual(st.__repr__(), str(st))
-
-    def test_start_and_stop(self):
-        cfg = orderly_web.read_config("example")
-        try:
             res = orderly_web.start(cfg)
-            self.assertTrue(res)
-            st = orderly_web.status(cfg)
-            self.assertEqual(st.containers["orderly"]["status"], "running")
-            self.assertEqual(st.containers["web"]["status"], "running")
-            self.assertEqual(st.volumes["orderly"]["status"], "created")
-            self.assertEqual(st.network["status"], "up")
+            msg = f.getvalue().strip()
+        assert not res
+        assert msg.endswith("please run orderly-web stop")
 
-            f = io.StringIO()
-            with redirect_stdout(f):
-                res = orderly_web.start(cfg)
-                msg = f.getvalue().strip()
-            self.assertFalse(res)
-            self.assertTrue(msg.endswith("please run orderly-web stop"))
+        web = cfg.get_container("web")
+        ports = web.attrs["HostConfig"]["PortBindings"]
+        assert list(ports.keys()) == ["8888/tcp"]
 
-            web = cfg.get_container("web")
-            ports = web.attrs["HostConfig"]["PortBindings"]
-            self.assertEqual(list(ports.keys()), ["8888/tcp"])
+        dat = json.loads(http_get("http://localhost:8888"))
+        assert dat["status"] == "success"
+        orderly_web.stop(cfg, kill=True, volumes=True, network=True)
+        st = orderly_web.status(cfg)
+        assert st.containers["orderly"]["status"] == "missing"
+        assert st.containers["web"]["status"] == "missing"
+        assert st.volumes["orderly"]["status"] == "missing"
+        assert st.network["status"] == "down"
+    finally:
+        orderly_web.stop(cfg, kill=True, volumes=True, network=True)
 
-            dat = json.loads(http_get("http://localhost:8888"))
-            self.assertEqual(dat["status"], "success")
-            orderly_web.stop(cfg, kill=True, volumes=True, network=True)
-            st = orderly_web.status(cfg)
-            self.assertEqual(st.containers["orderly"]["status"], "missing")
-            self.assertEqual(st.containers["web"]["status"], "missing")
-            self.assertEqual(st.volumes["orderly"]["status"], "missing")
-            self.assertEqual(st.network["status"], "down")
-        finally:
-            orderly_web.stop(cfg, kill=True, volumes=True, network=True)
-
-    def test_no_devmode_no_ports(self):
-        cfg = orderly_web.read_config("example")
-        cfg.web_dev_mode = False
-        try:
-            orderly_web.start(cfg)
-            web = cfg.get_container("web")
-            self.assertIsNone(web.attrs["HostConfig"]["PortBindings"])
-        finally:
-            orderly_web.stop(cfg, kill=True, volumes=True, network=True)
+def test_no_devmode_no_ports():
+    cfg = orderly_web.read_config("example")
+    cfg.web_dev_mode = False
+    try:
+        orderly_web.start(cfg)
+        web = cfg.get_container("web")
+        assert web.attrs["HostConfig"]["PortBindings"] is None
+    finally:
+        orderly_web.stop(cfg, kill=True, volumes=True, network=True)
 
 
 # Because we wait for a go signal to come up, we might not be able to
