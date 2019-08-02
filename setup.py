3dfef08dfccfc3808eb5caf9026017e5000bc341
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-08-02 16:50:47 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
     "Pillow"]
 
 setup(name="orderly_web",
-      version="0.0.2",
+      version="0.0.3",
       description="Deploy scripts for OrderlyWeb",
       long_description=long_description,
       classifiers=[
