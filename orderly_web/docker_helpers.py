af247daf7f7fb934f29b5cdb415412a87c288e76
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-12-13 17:06:58 +0000

    Less removal of close

--- orderly_web/docker_helpers.py
+++ orderly_web/docker_helpers.py
@@ -187,3 +187,6 @@ class docker_client():
     def __enter__(self):
         self.client = docker.client.from_env()
         return self.client
+
+    def __exit__(self, type, value, traceback):
+        pass
